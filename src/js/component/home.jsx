91a991164a67a95b41fdfd00f1e3e04a7e73e39c
holyfs <egcg1986@gmail.com>
2022-05-24 18:23:00 +0000

    agregada funcionalidad del boton cambio de color

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,30 +4,43 @@ import Semaforo from "./Semaforo.jsx";
 
 //create your first component
 const Home = () => {
-	const [glow1, setglow1] = useState(" ");
-	const [glow2, setglow2] = useState(" ");
-	const [glow3, setglow3] = useState(" ");
+	const [glow1, setglow1] = useState("");
+	const [glow2, setglow2] = useState("");
+	const [glow3, setglow3] = useState("");
 	const agregarGlow = (e) => {
 		if (e.target.className.startsWith("color1")) {
 			setglow1(" glow");
-			setglow2(" ");
-			setglow3(" ");
+			setglow2("");
+			setglow3("");
 		}
 		if (e.target.className.startsWith("color2")) {
-			setglow1(" ");
+			setglow1("");
 			setglow2(" glow");
-			setglow3(" ");
+			setglow3("");
 		}
 		if (e.target.className.startsWith("color3")) {
-			setglow1(" ");
-			setglow2(" ");
+			setglow1("");
+			setglow2("");
 			setglow3(" glow");
 		}
 	};
-
-	const cambiarGlow = (e) => {
-		console.log("entra");
+	let arrayOfFunctions = [glow1, glow2, glow3];
+	const cambiaDeColor = (arr) => {
+		if (glow1 === "" && glow2 === "" && glow3 === "") {
+			arr[0] = " glow";
+		}
+		for (let i = 0; i < arr.length; i++) {
+			if (i > 0) {
+				let el = arr[i];
+				arr[i] = arr[i - 1];
+				arr[i - 1] = el;
+			}
+		}
+		setglow1(arr[0]);
+		setglow2(arr[1]);
+		setglow3(arr[2]);
 	};
+
 	return (
 		<div>
 			<div className="stick"></div>
@@ -49,7 +62,7 @@ const Home = () => {
 				/>
 			</div>
 			<BotonDeCambio
-				onClick={cambiarGlow}
+				onClick={() => cambiaDeColor(arrayOfFunctions)}
 				className={"cambioDeColor"}
 				type={"button"}
 			/>
